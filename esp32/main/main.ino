439c554eb8d25232d27ad28a656f2c7a29c8b8da
aaryatopiwala <75400069+aaryatopiwala@users.noreply.github.com>
2025-03-05 01:01:21 -0800

    new branch and make delays nonblocking

--- esp32/main/main.ino
+++ esp32/main/main.ino
@@ -1,14 +1,97 @@
 #include <Arduino.h>
 #include <WiFi.h>
-
-#define TAPO_DEBUG_MODE // Comment this line to disable debug messages
 #include "tapo_device.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <ArduinoJson.h>
 
+#define LED_BUILTIN 2
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define ECHO_PIN 17
+#define TRIG_PIN 16
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define TAPO_DEBUG_MODE // Comment this line to disable debug messages
+#define TIMEOUT_PERIOD 15000 // 15 000 mill  = 15 seconds
 TapoDevice tapo;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+float timing = 0.0;
+float distance = 0.0;
+long int currTime = 0;
+float ledSwitch = 0.0;
+float ultrasonic = 0.0;
+long int elapsed = 0;
+int timeout = 0;
+String energy = "";
+String power = "";
+String energy_usage_day = "";
+String powerStr = "";
+String energyDayStr = "";
+int result_today_runtime = 0;
+int result_month_runtime = 0;
+int result_today_energy = 0;
+int result_month_energy = 0;
+
+int result_electricity_charge_0 = 0;
+int result_electricity_charge_1 = 0;
+int result_electricity_charge_2 = 0;
+const char* result_local_time = "";
+
+
+int result_current_power = 0;
+int tapoState;
+int error_code = 0;
+
+
+void check_distance_and_shutoff() {
+  if (distance > 10.0){
+    if (timeout != 1){ // timeout starts now!
+      elapsed = currTime;
+      timeout = 1;
+    }
+    else if (timeout == 1){
+      Serial.print("elapsed - currTime:");
+      Serial.println(currTime - elapsed);
+      if (currTime - elapsed > TIMEOUT_PERIOD && tapoState == 1){
+        tapo.off();
+        tapo.update();
+        Serial.println("Tapo Off");
+        if (tapo.getPriority() == 1) { 
+          tapoState = 0;
+        }
+        }
+    }
+  }
+  else {
+    if (timeout == 1) {
+      timeout = 0; //reset timeout
+      tapo.on();
+      tapo.update();
+      Serial.println("Tapo On");
+      tapoState = 1;
+    }
+  }
+}
 
 void setup() {
     Serial.begin(115200);
-
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;); // Don't proceed, loop forever
+    }
+    
     // Connect to WiFi
     WiFi.begin("RESNET-GUEST-DEVICE", "ResnetConnect");
     while (WiFi.status() != WL_CONNECTED) {
@@ -18,16 +101,88 @@ void setup() {
 
     // Initialize Tapo device
     tapo.begin("100.117.17.94", "aaryatopiwala@gmail.com", "TapoPassword123");
+    tapoState = 1;
+
+    tapo.on();
     // Example: tapo.begin("192.168.1.100", "abc@example.com", "abc123");
+
+    // Show initial display buffer contents on the screen --
+    // the library initializes this with an Adafruit splash screen.
+    display.display();
+    delay(2000); // Pause for 2 seconds
+
+    // Clear the buffer
+    display.clearDisplay();
 }
 
 void loop() {
-    //tapo.on(); // Turn on the device
-    //delay(5000);
-    //tapo.off(); // Turn off the device
-    Serial.print("Tapo Energy: ");
-    Serial.println(tapo.energy()); // get energy
-    Serial.print("Tapo Emeter: ");
-    Serial.println(tapo.emeter()); // get energy
-    delay(2000);
+  currTime = millis();
+  
+  digitalWrite(TRIG_PIN, LOW);
+  delay(2);  
+  digitalWrite(TRIG_PIN, HIGH);
+  delay(10);
+  digitalWrite(TRIG_PIN, LOW);
+  //tapo.update(); //updates everything in tapo_device.h
+  Serial.println("Tapo Updated");
+  timing = pulseIn(ECHO_PIN, HIGH);
+  distance = (timing * 0.034) / 2;
+  display.display();                   
+  display.clearDisplay();                                       
+  display.setTextSize(1); // Smaller text to fit all info
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+
+  
+  check_distance_and_shutoff();
+  
+    
+  //Serial.print("Tapo Energy: ");
+  tapo.energy();
+  tapo.update();
+  
+
+if (tapo.getPriority() == 0 && tapo.gotResponse()) { // if executing another request, don't update the values (otherwise they go to zero if on/off request is occurring.)
+    energy = tapo.getResponse(); // only update energy json if priority is 0
+
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, energy);
+
+    if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        return;
+    }
+
+    JsonObject result = doc["result"];
+    result_today_runtime = result["today_runtime"]; // 1064
+    result_month_runtime = result["month_runtime"]; // 5382
+    result_today_energy = result["today_energy"]; // 72
+    result_month_energy = result["month_energy"]; // 531
+    result_local_time = result["local_time"]; // "2025-03-04 17:44:35"
+    
+    JsonArray result_electricity_charge = result["electricity_charge"];
+    result_electricity_charge_0 = result_electricity_charge[0]; // 0
+    result_electricity_charge_1 = result_electricity_charge[1]; // 335
+    result_electricity_charge_2 = result_electricity_charge[2]; // 231
+    
+    result_current_power = result["current_power"]; // 19294
+    
+    error_code = doc["error_code"]; // 0
+}
+
+
+  
+
+display.print("Distance (cm): ");
+display.println(distance);
+display.print("Power (mW): ");
+display.println(result_current_power);
+display.print("Energy Today (Wh): ");
+display.println(result_today_energy);
+//display.print("Energy: ");
+//display.println(tapo.getResponse());
+display.display();
+    
+  
 }
--- esp32/main/tapo_device.h
+++ esp32/main/tapo_device.h
@@ -5,9 +5,24 @@
 #define TAPO_MAX_SEND_RETRIES 3
 #define TAPO_MAX_RECONNECT_RETRIES 3
 
+
+struct Command {
+    String command;
+    String expected_state;
+    String response;
+    int gotResponse;
+    int send_retries; // number of sends left
+    int reconnect_retries; // number of reconnects left
+    unsigned long last_retry_time;
+    int priority; // higher number = higher priority
+};
+
 class TapoDevice {
 private:
     TapoProtocol protocol;
+    Command currCommand;
+    bool commandActive = false;
+    unsigned int retryDelay = 1000;
 
     bool check_state(const String &expected_state) {
         String response = protocol.send_message("{\"method\":\"get_device_info\",\"params\":{}}");
@@ -21,17 +36,95 @@ private:
                     return true;
                 }
                 protocol.send_message(command);
-                delay(1000); // Wait for the command to be applied
+                //delay(1000); // Wait for the command to be applied
             }
-            Serial.println("TAPO_DEVICE: Failed to send command, rehandshaking...");
+            //Serial.println("TAPO_DEVICE: Failed to send command, rehandshaking...");
             protocol.rehandshake();
         }
-        Serial.println("TAPO_DEVICE: Failed to rehandshake, giving up command");
+        //Serial.println("TAPO_DEVICE: Failed to rehandshake, giving up command");
         
         return false;
     }
 
 public:
+
+    void update() { // needs to be called in main loop to progress commands
+        if (commandActive) {
+            Serial.print("TAPO_DEVICE: Command active: "); 
+            Serial.print(currCommand.command);
+            Serial.print(" Expected state: ");
+            Serial.print(currCommand.expected_state);
+            Serial.print("Response: ");
+            Serial.print(currCommand.response);
+            Serial.print("Send retries: ");
+            Serial.print(currCommand.send_retries);
+            Serial.print("Reconnect retries: ");
+            Serial.print(currCommand.reconnect_retries);
+            Serial.print("Last retry time: ");
+            Serial.print(currCommand.last_retry_time);
+            Serial.print("Priority: ");
+            Serial.println(currCommand.priority);
+
+            if (millis() - currCommand.last_retry_time > retryDelay) {
+                if (currCommand.send_retries > 0) { //try sending multiple requests
+                    currCommand.send_retries--;
+                    currCommand.response = protocol.send_message(currCommand.command);
+                    currCommand.last_retry_time = millis();
+                    if (currCommand.expected_state == "" || check_state(currCommand.expected_state)) { 
+                        commandActive = false;
+                        currCommand.gotResponse = 1;
+                    } else {
+                        //Serial.println("TAPO_DEVICE: Command failed, retrying...");
+                    }
+                } else { // if all requests fail, try reconnecting
+                    if (currCommand.reconnect_retries > 0) {
+                        currCommand.reconnect_retries--;
+                        //Serial.println("TAPO_DEVICE: Command failed, rehandshaking...");
+                        protocol.rehandshake();
+                        //Serial.println("TAPO_DEVICE: Command failed, rehandshaking done");
+                        currCommand.last_retry_time = millis();
+                        currCommand.send_retries = TAPO_MAX_SEND_RETRIES; // reset the number of send retries after reconnecting
+                    } else {
+                        //Serial.println("TAPO_DEVICE: Command failed, giving up...");
+                        commandActive = false;
+                    }
+                }
+            }
+        } else {
+            //Serial.println("TAPO_DEVICE: Command inactive");
+        }
+    }
+
+    void sendCommand(const String &command, const String &expected_state = "", int priority = 0) {
+        //if (!commandActive) {
+        //if (millis() - currCommand.last_retry_time > retryDelay) {
+        if (priority > currCommand.priority || !commandActive) {
+            currCommand.command = command;
+            currCommand.expected_state = expected_state;
+            currCommand.send_retries = TAPO_MAX_SEND_RETRIES;
+            currCommand.reconnect_retries = TAPO_MAX_RECONNECT_RETRIES;
+            currCommand.last_retry_time = millis();
+            currCommand.priority = priority;
+            currCommand.response = "";
+            currCommand.gotResponse = 0;
+            commandActive = true;
+        //} else {
+            //Serial.println("TAPO_DEVICE: Command not sent - not enough time has passed since last command attempt.");
+        }
+    }
+    
+    String getResponse() {
+        return currCommand.response;
+    }
+
+    int getPriority() {
+        return currCommand.priority;
+    }
+
+    int gotResponse() {
+        return currCommand.gotResponse;
+    }
+
     void begin(const String& ip_address, const String& username, const String& password) {
         protocol.handshake(ip_address, username, password);
     }
@@ -39,48 +132,61 @@ public:
     void on() {
         String command = "{\"method\":\"set_device_info\",\"params\":{\"device_on\":true}}";
         String expected_state = "\"device_on\":true";
-        send_command(command, expected_state);
+        sendCommand(command, expected_state,2);
+        update();
+        //send_command(command, expected_state);
     }
 
     void off() {
         String command = "{\"method\":\"set_device_info\",\"params\":{\"device_on\":false}}";
         String expected_state = "\"device_on\":false";
-        send_command(command, expected_state);
+        sendCommand(command, expected_state,1);
+        update();
+        //send_command(command, expected_state);
     }
 
     String energy() {
         String command = "{\"method\":\"get_energy_usage\",\"params\":{}}";
-        //String expected_state = "";
-        for (int i = 0; i < TAPO_MAX_RECONNECT_RETRIES; i++) {
-            for (int j = 0; j < TAPO_MAX_SEND_RETRIES; j++) {
-                String response = protocol.send_message(command);
-                delay(1000); // Wait for the command to be applied
-
-
-                return response;
-            }
-            Serial.println("TAPO_DEVICE: Failed to send command, rehandshaking...");
-            protocol.rehandshake();
-        }
-        Serial.println("TAPO_DEVICE: Failed to rehandshake, giving up command");
-        return "";
+        //Serial.println("TAPO_DEVICE: Sending energy command");
+        String expected_state = "";
+        sendCommand(command, expected_state,0);
+        update();
+        //Serial.println("TAPO_DEVICE: Energy command sent");
+        return currCommand.response;
     }
+    // String energy() {
+    //     String command = "{\"method\":\"get_energy_usage\",\"params\":{}}";
+    //     //String expected_state = "";
+    //     for (int i = 0; i < TAPO_MAX_RECONNECT_RETRIES; i++) {
+    //         for (int j = 0; j < TAPO_MAX_SEND_RETRIES; j++) {
+    //             String response = protocol.send_message(command);
+    //             delay(1000); // Wait for the command to be applied
 
-    String emeter() {
-        String command = "{\"method\":\"get_emeter_usage\",\"params\":{}}";
-        //String expected_state = "";
-        for (int i = 0; i < TAPO_MAX_RECONNECT_RETRIES; i++) {
-            for (int j = 0; j < TAPO_MAX_SEND_RETRIES; j++) {
-                String response = protocol.send_message(command);
-                delay(1000); // Wait for the command to be applied
+
+    //             return response;
+    //         }
+    //         Serial.println("TAPO_DEVICE: Failed to send command, rehandshaking...");
+    //         protocol.rehandshake();
+    //     }
+    //     Serial.println("TAPO_DEVICE: Failed to rehandshake, giving up command");
+    //     return "";
+    // }
+
+    // String emeter() {
+    //     String command = "{\"method\":\"get_emeter_usage\",\"params\":{}}";
+    //     //String expected_state = "";
+    //     for (int i = 0; i < TAPO_MAX_RECONNECT_RETRIES; i++) {
+    //         for (int j = 0; j < TAPO_MAX_SEND_RETRIES; j++) {
+    //             String response = protocol.send_message(command);
+    //             delay(1000); // Wait for the command to be applied
 
 
-                return response;
-            }
-            Serial.println("TAPO_DEVICE: Failed to send command, rehandshaking...");
-            protocol.rehandshake();
-        }
-        Serial.println("TAPO_DEVICE: Failed to rehandshake, giving up command");
-        return "";
-    }
+    //             return response;
+    //         }
+    //         Serial.println("TAPO_DEVICE: Failed to send command, rehandshaking...");
+    //         protocol.rehandshake();
+    //     }
+    //     Serial.println("TAPO_DEVICE: Failed to rehandshake, giving up command");
+    //     return "";
+    // }
 };
