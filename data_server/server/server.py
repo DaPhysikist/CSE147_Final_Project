9f5cb9dd1cbfb2ea69f83dd0bb81214575d92d96
Pranav <pvmehta936@gmail.com>
2025-03-08 12:45:34 -0800

    can successfully retrieve json payload from rpi web server

--- data_server/server/server.py
+++ data_server/server/server.py
@@ -8,102 +8,155 @@ import mysql.connector as mysql
 from dotenv import load_dotenv
 
 load_dotenv()
-
-# Read Database connection variables
 db_host = "localhost"
 db_user = "root"
 db_pass = os.environ['MYSQL_ROOT_PASSWORD']
 db_name = "ShutEyeDataServer"                           
 
-app = FastAPI()     #initialize the web app
-app.mount("/static", StaticFiles(directory="static"), name="static")    #mounts the public folder with static files to the app
+app = FastAPI()
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)       #returns the index HTML page for the default URL path
 def get_html() -> HTMLResponse:                 
   with open('index.html') as html:             
     return HTMLResponse(content=html.read())
   
-@app.get("/dashboard", response_class=HTMLResponse)       #returns the dashboard HTML page when dashboard is specified for the URL path
-def get_dashboard() -> HTMLResponse:
-  with open('dashboard.html') as html:          
-    return HTMLResponse(content=html.read())
+# @app.get("/dashboard", response_class=HTMLResponse)       #returns the dashboard HTML page when dashboard is specified for the URL path
+# def get_dashboard() -> HTMLResponse:
+#   with open('dashboard.html') as html:          
+#     return HTMLResponse(content=html.read())
 
-@app.get("/load_dashboard", response_class=JSONResponse)   #used to load the data from the database into a JSON file -> used to display the idea data on the dashboard
-def load_dashboard() -> JSONResponse:
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-  cursor.execute("select * from Ideas;")
-  records = cursor.fetchall()
-  db.close()
-  response = {}
-  for index, row in enumerate(records):    #iterates through the database data to construct the dict
-    response[index] = {
-      "id": row[0],
-      "first_name": row[1],
-      "last_name": row[2],
-      "summary": row[3],
-      "jtbd": row[4],
-      "competitors": row[5],
-      "price": float(row[6]),
-      "cost": float(row[7])
-    }
-  return JSONResponse(response)                  
+@app.get("/shuteye_historical_data/{appliance_name}", response_class=JSONResponse)
+def fetch_historical_data(appliance_name: str) -> JSONResponse:
+    try:
+        db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+        cursor = db.cursor()
+        query = "SELECT * FROM ShutEyeDeviceEnergyDataHistorical WHERE appliance_name = %s ORDER BY local_time DESC;"
+        value = (appliance_name,)
+        cursor.execute(query, value)
+        records = cursor.fetchall()
+        response = {}
+        for index, row in enumerate(records):  # Iterate through the database data to construct the dict
+            response[index] = {
+                "appliance_name": row[0],
+                "local_time": row[1].strftime("%Y-%m-%d %H:%M:%S"),
+                "today_runtime": row[2],
+                "month_runtime": row[3],
+                "today_energy": row[4],
+                "month_energy": row[5]
+            }
+        db.close()
+        return JSONResponse(content=response)
+    except mysql.Error as err:
+        return JSONResponse(status_code=500, content={"error": f"Database error: {err}"})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": f"An error occurred: {e}"})
+    
+
+@app.get("/shuteye_periodic_measurement_data/{appliance_name}", response_class=JSONResponse)
+def fetch_periodic_measurement_data(appliance_name: str) -> JSONResponse:
+    try:
+        db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+        cursor = db.cursor()
+        query = "SELECT * FROM ShutEyeDeviceEnergyDataPeriodicMeasurement WHERE appliance_name = %s ORDER BY local_time DESC;"
+        value = (appliance_name,)
+        cursor.execute(query, value)
+        records = cursor.fetchall()
+        response = {}
+        for index, row in enumerate(records):  # Iterate through the database data to construct the dict
+            response[index] = {
+                "appliance_name": row[0],
+                "local_time": row[1].strftime("%Y-%m-%d %H:%M:%S"),
+                "current_power": row[2],
+                "distance_ultrasonic": row[3],
+                "distance_bluetooth": row[4],
+                "distance_ultrawideband": row[5],
+                "user_presence_detected": row[6],
+            }
+        db.close()
+        return JSONResponse(content=response)
+    except mysql.Error as err:
+        return JSONResponse(status_code=500, content={"error": f"Database error: {err}"})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": f"An error occurred: {e}"})
+
+
+# @app.get("/shuteye_periodic_measurement_data", response_class=JSONResponse)
+# def fetch_periodic_measurement_data() -> JSONResponse:
+#   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+#   cursor = db.cursor()
+#   cursor.execute("select * from ShutEyeDeviceEnergyDataPeriodicMeasurement;")
+#   records = cursor.fetchall()
+#   db.close()
+#   response = {}
+#   for index, row in enumerate(records):    #iterates through the database data to construct the dict
+#     response[index] = {
+#       "id": row[0],
+#       "first_name": row[1],
+#       "last_name": row[2],
+#       "summary": row[3],
+#       "jtbd": row[4],
+#       "competitors": row[5],
+#       "price": float(row[6]),
+#       "cost": float(row[7])
+#     }
+#   return JSONResponse(response)    
   
-@app.post("/idea/{idea_id}")       #modifies an idea in the database
-def modify_idea(idea_id: str, parameters: dict):
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-  field = parameters['field']
-  set_val = parameters['set_val']
-  if field == 'first_name':                                   #update the value of the selected field
-      query = "update Ideas set first_name=%s WHERE id=%s;"
-  elif field == 'last_name':
-      query = "update Ideas set last_name=%s WHERE id=%s;"
-  elif field == 'summary':
-      query = "update Ideas set summary=%s WHERE id=%s;"
-  elif field == 'jtbd':
-      query = "update Ideas set jtbd=%s WHERE id=%s;"
-  elif field == 'competitors':
-      query = "update Ideas set competitors=%s WHERE id=%s;"
-  elif field == 'price':
-      query = "update Ideas set price=%s WHERE id=%s;"
-  elif field == 'cost':
-      query = "update Ideas set cost=%s WHERE id=%s;"
-  else:
-     return
-  value = (set_val, idea_id)
-  try:
-    cursor.execute(query, value)
-  except RuntimeError as err:
-    print("runtime error: {0}".format(err))
-  db.commit()
-  db.close()
+# @app.post("/idea/{idea_id}")       #modifies an idea in the database
+# def modify_idea(idea_id: str, parameters: dict):
+#   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+#   cursor = db.cursor()
+#   field = parameters['field']
+#   set_val = parameters['set_val']
+#   if field == 'first_name':                                   #update the value of the selected field
+#       query = "update Ideas set first_name=%s WHERE id=%s;"
+#   elif field == 'last_name':
+#       query = "update Ideas set last_name=%s WHERE id=%s;"
+#   elif field == 'summary':
+#       query = "update Ideas set summary=%s WHERE id=%s;"
+#   elif field == 'jtbd':
+#       query = "update Ideas set jtbd=%s WHERE id=%s;"
+#   elif field == 'competitors':
+#       query = "update Ideas set competitors=%s WHERE id=%s;"
+#   elif field == 'price':
+#       query = "update Ideas set price=%s WHERE id=%s;"
+#   elif field == 'cost':
+#       query = "update Ideas set cost=%s WHERE id=%s;"
+#   else:
+#      return
+#   value = (set_val, idea_id)
+#   try:
+#     cursor.execute(query, value)
+#   except RuntimeError as err:
+#     print("runtime error: {0}".format(err))
+#   db.commit()
+#   db.close()
 
-@app.delete("/deleteidea/{idea_id}") #deletes an idea from the database
-def delete_idea(idea_id: str):
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-  query = "delete from Ideas where id = %s;"
-  value = (idea_id,)
-  try:
-    cursor.execute(query, value)
-  except RuntimeError as err:
-    print("runtime error: {0}".format(err))
-  db.commit()
-  db.close()
+# @app.delete("/deleteidea/{idea_id}") #deletes an idea from the database
+# def delete_idea(idea_id: str):
+#   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+#   cursor = db.cursor()
+#   query = "delete from Ideas where id = %s;"
+#   value = (idea_id,)
+#   try:
+#     cursor.execute(query, value)
+#   except RuntimeError as err:
+#     print("runtime error: {0}".format(err))
+#   db.commit()
+#   db.close()
 
-@app.put("/addidea")       #adds an idea in the database
-def add_idea(idea_data: dict):
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-  query = "insert into Ideas(first_name, last_name, summary, jtbd, competitors, price, cost) values (%s, %s, %s, %s, %s, %s, %s)"
-  value = (idea_data['first_name'], idea_data['last_name'], idea_data['summary'], idea_data['jtbd'], idea_data['competitors'], idea_data['price'], idea_data['cost'])  #uses dict passed in to PUT to set values of fields
-  try:
-    cursor.execute(query, value)
-  except RuntimeError as err:
-    print("runtime error: {0}".format(err))
-  db.commit()
-  db.close()
+# @app.put("/addidea")       #adds an idea in the database
+# def add_idea(idea_data: dict):
+#   db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+#   cursor = db.cursor()
+#   query = "insert into Ideas(first_name, last_name, summary, jtbd, competitors, price, cost) values (%s, %s, %s, %s, %s, %s, %s)"
+#   value = (idea_data['first_name'], idea_data['last_name'], idea_data['summary'], idea_data['jtbd'], idea_data['competitors'], idea_data['price'], idea_data['cost'])  #uses dict passed in to PUT to set values of fields
+#   try:
+#     cursor.execute(query, value)
+#   except RuntimeError as err:
+#     print("runtime error: {0}".format(err))
+#   db.commit()
+#   db.close()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=6543)     #starts the app
+    uvicorn.run(app, host="0.0.0.0", port=6543)
--- data_server/server/test_data.sql
+++ data_server/server/test_data.sql
@@ -1,15 +1,15 @@
 INSERT INTO ShutEyeDeviceEnergyDataHistorical (appliance_name, local_time, today_runtime, month_runtime, today_energy, month_energy)
 VALUES 
-('Appliance1', '2025-01-01 08:00:00', 120, 1500, 300, 4500),
-('Appliance1', '2025-01-02 09:00:00', 100, 1600, 250, 4600),
-('Appliance2', '2025-01-01 10:00:00', 90, 1300, 200, 4000),
-('Appliance3', '2025-01-01 11:00:00', 130, 1400, 280, 4200);
+('microwave', '2025-01-01 08:00:00', 120, 1500, 300, 4500),
+('microwave', '2025-01-02 09:00:00', 100, 1600, 250, 4600),
+('blender', '2025-01-01 10:00:00', 90, 1300, 200, 4000),
+('toaster', '2025-01-01 11:00:00', 130, 1400, 280, 4200);
 
 INSERT INTO ShutEyeDeviceEnergyDataPeriodicMeasurement (appliance_name, local_time, current_power, distance_ultrasonic, distance_bluetooth, distance_ultrawideband, user_presence_detected)
 VALUES 
-('Appliance1', '2025-01-01 08:00:00', 150, 20, 30, 50, TRUE),
-('Appliance2', '2025-01-01 09:00:00', 120, 25, 35, 45, FALSE),
-('Appliance3', '2025-01-01 10:00:00', 180, 15, 25, 40, TRUE);
+('microwave', '2025-01-01 08:00:00', 150, 20, 30, 50, TRUE),
+('blender', '2025-01-01 09:00:00', 120, 25, 35, 45, FALSE),
+('toaster', '2025-01-01 10:00:00', 180, 15, 25, 40, TRUE);
 
 
 SELECT * FROM ShutEyeDeviceEnergyDataHistorical;
